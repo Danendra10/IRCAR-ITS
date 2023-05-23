eadc54f1dc771c5026cf159a06440da166551571
Dan Mac <danendraclever24@gmail.com>
2023-05-24 03:01:02 +0700

    polynomial regression

--- src/utils/include/math/math.hh
+++ src/utils/include/math/math.hh
@@ -1,8 +1,27 @@
 #ifndef __MATH_HH_
 #define __MATH_HH_
 
+#include "opencv2/core/core.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
 unsigned int DivideBy2(unsigned int in);
 unsigned int DivideBy3(unsigned int in);
 unsigned int DivideBy4(unsigned int in);
+
+class PolynomialRegression
+{
+public:
+    PolynomialRegression(int degree);
+    void fit(const vector<double> &x, const vector<double> &y);
+    double predict(double x);
+    void print();
+
+private:
+    int degree;
+    vector<double> w;
+};
 
 #endif
--- src/utils/src/math.cc
+++ src/utils/src/math.cc
@@ -18,3 +18,61 @@ unsigned int DivideBy4(unsigned int in)
 {
     return in >> 2;
 }
+
+PolynomialRegression::PolynomialRegression(int degree)
+{
+    this->degree = degree;
+    w = vector<double>(degree + 1, 0);
+}
+
+void PolynomialRegression::fit(const vector<double> &x, const vector<double> &y)
+{
+    int n = x.size();
+    Mat X = Mat::zeros(n, degree + 1, CV_64FC1);
+    Mat Y = Mat::zeros(n, 1, CV_64FC1);
+    Mat W = Mat::zeros(degree + 1, degree + 1, CV_64FC1);
+    Mat W_inv = Mat::zeros(degree + 1, degree + 1, CV_64FC1);
+    Mat W_inv_X = Mat::zeros(degree + 1, n, CV_64FC1);
+    Mat W_inv_X_Y = Mat::zeros(degree + 1, 1, CV_64FC1);
+
+    for (int i = 0; i < n; i++)
+    {
+        Y.at<double>(i, 0) = y[i];
+        for (int j = 0; j < degree + 1; j++)
+        {
+            X.at<double>(i, j) = pow(x[i], j);
+        }
+    }
+
+    W = X.t() * X;
+    W_inv = W.inv();
+    W_inv_X = W_inv * X.t();
+    W_inv_X_Y = W_inv_X * Y;
+
+    for (int i = 0; i < degree + 1; i++)
+    {
+        w[i] = W_inv_X_Y.at<double>(i, 0);
+    }
+}
+
+double PolynomialRegression::predict(double x)
+{
+    double y = 0;
+    for (int i = 0; i < degree + 1; i++)
+    {
+        y += w[i] * pow(x, i);
+    }
+    return y;
+}
+
+void PolynomialRegression::print()
+{
+    cout << "y = ";
+    for (int i = degree; i >= 0; i--)
+    {
+        cout << w[i] << "x^" << i;
+        if (i != 0)
+            cout << " + ";
+    }
+    cout << endl;
+}
